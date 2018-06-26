2567419cd706e8e77b9108c547e7039829d0d683
Magnus Akselvoll <magnus.akselvoll@ibistic.com>
2018-06-26 16:25:03 +0200

    Added support for mapping airports to countries

--- Ibistic.Public.OpenAirportData/Ibistic.Public.OpenAirportData/OpenFlightsData/OpenFlightsDataAirportProvider.cs
+++ Ibistic.Public.OpenAirportData/Ibistic.Public.OpenAirportData/OpenFlightsData/OpenFlightsDataAirportProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using CsvHelper;
 using CsvHelper.Configuration;
 
@@ -9,12 +10,14 @@ namespace Ibistic.Public.OpenAirportData.OpenFlightsData
 {
     public sealed class OpenFlightsDataAirportProvider : OpenFlightsDataProvider
     {
+        private readonly OpenFlightsDataCountryProvider _countryProvider;
         private FileStream _readerFileStream;
         private TextReader _textReader;
         private CsvReader _csvReader;
 
-        public OpenFlightsDataAirportProvider(string cacheFileName) : base(cacheFileName)
+        public OpenFlightsDataAirportProvider(string cacheFileName, OpenFlightsDataCountryProvider countryProvider = null) : base(cacheFileName)
         {
+            _countryProvider = countryProvider;
             Source = new Uri("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat");
         }
 
@@ -23,6 +26,7 @@ namespace Ibistic.Public.OpenAirportData.OpenFlightsData
         public override void ClearCache()
         {
             CloseReaders();
+            _countryProvider?.ClearCache();
 
             base.ClearCache();
         }
@@ -46,7 +50,29 @@ namespace Ibistic.Public.OpenAirportData.OpenFlightsData
             configuration.BadDataFound = context => BadDataRowCount++;
 
             _csvReader = new CsvReader(_textReader, configuration);
-            return _csvReader.GetRecords<Airport>();
+            var airports =  _csvReader.GetRecords<Airport>();
+
+            return _countryProvider == null ? airports : AddCountryInformation(airports);
+        }
+
+        private IEnumerable<Airport> AddCountryInformation(IEnumerable<Airport> airports)
+        {
+            Dictionary<string, Country> countriesByName = _countryProvider.GetAllCountries().Where(x => !String.IsNullOrEmpty(x.Name))
+                .ToDictionary(x => x.Name);
+
+            foreach (var airport in airports)
+            {
+                if (String.IsNullOrEmpty(airport.CountryAlpha2) && String.IsNullOrEmpty(airport.CountryAlpha3) && !String.IsNullOrEmpty(airport.CountryName))
+                {
+                    if (countriesByName.TryGetValue(airport.CountryName, out Country country))
+                    {
+                        airport.CountryAlpha2 = country.Alpha2;
+                        airport.CountryAlpha3 = country.Alpha3;
+                    }
+                }
+
+                yield return airport;
+            }
         }
 
         protected override void Dispose(bool disposing)
